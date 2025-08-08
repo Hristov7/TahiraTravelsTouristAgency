08e9be07b0daea475421a70294f7ca6a6cf2b452
Hristov7 <danielantonov07@gmail.com>
2025-08-08 04:52:23 +0300

    ICategoryService and CategoryService added, ViewModel added. Tour Destinations variety of Categories.

---
+++ Core/CategoryService.cs
@@ -0,0 +1,28 @@
+﻿using Core.Contracts;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using ViewModels.ViewModels;
+
+namespace Core
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly TahiraTravelsDbContext dbContext;
+
+        public CategoryService(TahiraTravelsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<CategoryViewModel>> GetAllCategoriesAsync()
+        {
+            return await dbContext.Categories
+                .Select(c => new CategoryViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name
+                })
+                .ToListAsync();
+        }
+    }
+}
---
+++ Core/Contracts/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using ViewModels.ViewModels;
+
+namespace Core.Contracts
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryViewModel>> GetAllCategoriesAsync();
+    }
+}
